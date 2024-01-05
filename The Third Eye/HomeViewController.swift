6d162f763df09f944f3d92eb5c1832850e9b026a
abhinavbulusu <abhibulls001@gmail.com>
2024-01-04 18:49:37 -0600

    Push latest

--- The Third Eye/HomeViewController.swift
+++ The Third Eye/HomeViewController.swift
@@ -23,7 +23,7 @@ class HomeViewController: UIViewController, VNDocumentCameraViewControllerDelega
     private var searchViewController:SearchViewController?
     private var defaults = UserDefaults.standard
     private var screenWidth = UIScreen.main.bounds.size.width
-    private var screenHeight = UIScreen.main.bounds.size.height
+    private var screenHeight = UIScreen.main.bounds.size.height 
 
     
     
